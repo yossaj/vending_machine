b6f18231bc57bc64197eb332083b2096b92c1b30
James Swatton <jamessswatton@gmail.com>
2019-06-08 17:46:35 +0100

    basic spec for washine_machine model set up. mocha installed

---
+++ client/src/model/vending_machine.js
@@ -0,0 +1,11 @@
+const VendingMachine = function () {
+  this.allCoins = [];
+  this.currentCoins = [];
+  this.balance = 0;
+};
+
+VendingMachine.prototype.insertCoin = function () {
+
+};
+
+module.exports = VendingMachine;
---
+++ client/src/specs/vending_machine_spec.js
@@ -0,0 +1,6 @@
+const assert = require('assert');
+const VendingMachine = require('../model/vending_machine.js');
+
+describe('VendingMachine', function () {
+
+});
