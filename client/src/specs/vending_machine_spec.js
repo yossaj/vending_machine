30f2ac76619a05481738277ff737250ee590a8eb
James Swatton <jamessswatton@gmail.com>
2019-06-08 18:13:25 +0100

    develop branch created. test created in vending spec

--- client/src/specs/vending_machine_spec.js
+++ client/src/specs/vending_machine_spec.js
@@ -2,5 +2,10 @@ const assert = require('assert');
 const VendingMachine = require('../model/vending_machine.js');
 
 describe('VendingMachine', function () {
-
+  it('should be able to store a coin', function () {
+    const vendingMachine = new VendingMachine ();
+    coin = {type: 'dime', value: 10}
+    vendingMachine.insertCoin(coin)
+    assert.deepStrictEqual(vendingMachine.currentCoins,[{type: 'dime', value: 10}])
+  })
 });
