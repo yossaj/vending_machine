2d577e34a7352ad2c4de8e6e518781bb700d66af
James Swatton <jamessswatton@gmail.com>
2019-06-09 15:00:32 +0100

    add should be able to add coin value to current balance to vending specs

--- client/src/specs/vending_machine_spec.js
+++ client/src/specs/vending_machine_spec.js
@@ -2,10 +2,28 @@ const assert = require('assert');
 const VendingMachine = require('../model/vending_machine.js');
 
 describe('VendingMachine', function () {
-  it('should be able to store a coin', function () {
-    const vendingMachine = new VendingMachine ();
-    coin = {type: 'dime', value: 10}
-    vendingMachine.insertCoin(coin)
-    assert.deepStrictEqual(vendingMachine.currentCoins,[{type: 'dime', value: 10}])
-  })
+  describe('VendingMachine', function () {
+
+    beforeEach(function () {
+      const coin1 = {type: 'nickel', value: 5};
+      const coin2 = {type: 'dime', value: 10};
+      const coin3 = {type: 'quarter', value: 25}
+      const coin4 = {type: 'half dollar', value: 50}
+      const coin5 = {type: 'dollar', value: 50}
+
+      const vendingMachine = new VendingMachine ();
+
+    })
+
+    it('should be able to store a coin', function () {
+      vendingMachine.insertCoin(coin1)
+      assert.deepStrictEqual(vendingMachine.currentCoins,[{type: 'dime', value: 10}])
+    })
+
+    it('should be able to add the coins value to current balance', function () {
+      
+    })
+
+  });
+
 });
