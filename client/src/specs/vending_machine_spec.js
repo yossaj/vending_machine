d714420a1fe4b9ea1fcd2bfdea4735d803d8c9fd
James Swatton <jamessswatton@gmail.com>
2019-06-09 15:06:00 +0100

    add 'should be able to return current coins' to vending specs

--- client/src/specs/vending_machine_spec.js
+++ client/src/specs/vending_machine_spec.js
@@ -7,9 +7,9 @@ describe('VendingMachine', function () {
     beforeEach(function () {
       const coin1 = {type: 'nickel', value: 5};
       const coin2 = {type: 'dime', value: 10};
-      const coin3 = {type: 'quarter', value: 25}
-      const coin4 = {type: 'half dollar', value: 50}
-      const coin5 = {type: 'dollar', value: 50}
+      const coin3 = {type: 'quarter', value: 25};
+      const coin4 = {type: 'half dollar', value: 50};
+      const coin5 = {type: 'dollar', value: 50};
 
       const vendingMachine = new VendingMachine ();
 
@@ -17,13 +17,20 @@ describe('VendingMachine', function () {
 
     it('should be able to store a coin', function () {
       vendingMachine.insertCoin(coin1)
-      assert.deepStrictEqual(vendingMachine.currentCoins,[{type: 'dime', value: 10}])
+      assert.deepStrictEqual(vendingMachine.currentCoins,[{type: 'nickel', value: 5}])
     })
 
-    it('should be able to add the coins value to current balance', function () {
-      
+    it('should be able to add coin value to current balance', function () {
+      vendingMachine.insertCoin(coin1)
+      assert.deepStrictEqual(vendingMachine.balance, 5)
     })
 
+    it('should be able to return current coins', function () {
+      vendingMachine.insertCoin(coin1);
+      vendingMachine.insertCoin(coin2);
+      vendingMachine.insertCoin(coin3);
+      assert.deepStrictEqual(vendingMachine.returnCoins, [{type: 'nickel', value: 5}, {type: 'dime', value: 10}, {type: 'quarter', value: 25}])
+    })
   });
 
 });
