216b0c22e0aae67e0b9ff0da2c778d4ae4ce21c9
yossaj <yossaj@icloud.com>
2019-06-11 22:01:12 +0100

    change css and html of input - new number display - added more logic to input view

--- client/src/views/input_view.js
+++ client/src/views/input_view.js
@@ -6,11 +6,29 @@ const InputView = function(){
 
 InputView.prototype.bindEvents = function(){
 
-    const numGrid = document.querySelectorAll('.dialPad')
+    const firstNum = document.querySelector('#first-num')
+    const secondNum = document.querySelector('#second-num')
+    const thirdNum = document.querySelector('#third-num')
 
+    const numGrid = document.querySelectorAll('.dialPad')
     for (button of numGrid) {
         button.addEventListener('click', (evt) => {
-           PubSub.publish("InputView: Selected Item Value", (evt.target.innerHTML))
+            let selectString = evt.target.innerHTML
+
+            if(firstNum.textContent == 0){
+            firstNum.textContent = selectString;
+        }else if(secondNum.textContent == 0){
+            secondNum.textContent = selectString
+        }else if(thirdNum.textContent == 0){
+            thirdNum.textContent = selectString
+        }else{
+            firstNum.textContent = 0
+            secondNum.textContent = 0
+            thirdNum.textContent = 0
+        }
+            console.log(firstNum);
+            
+        //    PubSub.publish("InputView: Selected Item Value", (selectValue))
         })
     }
 }
