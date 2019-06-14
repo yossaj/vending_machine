c1be3ec58f5c24a6b71eb122eda49f851928f104
yossaj <yossaj@icloud.com>
2019-06-14 15:01:02 +0100

    muddled

--- client/src/views/api_display_view.js
+++ client/src/views/api_display_view.js
@@ -9,20 +9,35 @@ ApiDisplayView.prototype.bindEvents = function(){
  
 
     PubSub.subscribe('Data: Api data', (evt)=>{
-        const url = evt
-        console.log(url);
+        const url = evt.detail.message
+        // console.log(url);
         
 
-        const display = document.querySelector('.vending-machine')
+        const display = document.querySelector('#enter')
         display.addEventListener('click', (evt)=>{
-            
             this.render(url)
         })
     });
 }
 
-ApiDisplayView.prototype.render = function(details){
-    console.log(details)
+ApiDisplayView.prototype.render = function(url){
+
+    PubSub.subscribe('VendingMachine: item', (evt) =>{
+        if(evt){
+    const display = document.querySelector('.vending-machine')
+    display.style.display = 'none'
+    const vended = document.querySelector('.vended-item')
+    vended.style.display = 'inherit'}
+})
+    
+    // display.classList.add('display-none')
+
+    // const display = document.querySelector('#mainArticle')
+    // const image = document.createElement('img')
+    // image.src = url
+    // display.append(image)
+
+
 
 }
 
